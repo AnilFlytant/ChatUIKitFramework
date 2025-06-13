2e4e3a49a17215c32e0a294beb3f6b46ba09f6fe
Flytant <flytant@gmail.com>
2025-06-13 11:27:58 +0530

    fixed cell error

--- Sources/ChatUIKitFramework/ChatUIKitFramework.swift
+++ Sources/ChatUIKitFramework/ChatUIKitFramework.swift
@@ -3,7 +3,9 @@
 // ChatUIKitFramework.swift
 // A UIKit-based reusable chat UI framework compatible with Swift projects
 
+import Foundation
 import UIKit
+
 
 public protocol ChatUIKitDelegate: AnyObject {
     func didSendMessage(_ message: String)
@@ -66,7 +68,7 @@ public class ChatUIKitViewController: UIViewController, UITableViewDelegate, UIT
         NSLayoutConstraint.activate([
             inputContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             inputContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            inputContainerBottomConstraint,
+            inputContainerBottomConstraint!,
             inputContainer.heightAnchor.constraint(equalToConstant: 50),
 
             inputTextView.leadingAnchor.constraint(equalTo: inputContainer.leadingAnchor, constant: 8),
@@ -137,44 +139,49 @@ public class ChatUIKitViewController: UIViewController, UITableViewDelegate, UIT
     }
 }
 
-//public class ChatMessageCell: UITableViewCell {
-//    private let messageLabel = UILabel()
-//
-//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//        messageLabel.numberOfLines = 0
-//        messageLabel.font = .systemFont(ofSize: 16)
-//        messageLabel.textColor = .black
-//        messageLabel.backgroundColor = UIColor.lightGray.withAlphaComponent(0.3)
-//        messageLabel.layer.cornerRadius = 10
-//        messageLabel.layer.masksToBounds = true
-//        contentView.addSubview(messageLabel)
-//        messageLabel.translatesAutoresizingMaskIntoConstraints = false
-//
-//        NSLayoutConstraint.activate([
-//            messageLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-//            messageLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
-//            messageLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-//            messageLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
-//        ])
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    func configure(with message: ChatMessage) {
-//        messageLabel.text = message.text
-//        messageLabel.textAlignment = message.isUser ? .right : .left
-//    }
-//}
-//
-//public struct ChatMessage {
-//    public let text: String
-//    public let isUser: Bool
-//
-//    public init(text: String, isUser: Bool) {
-//        self.text = text
-//        self.isUser = isUser
-//    }
-//}
+class ChatMessageCell: UITableViewCell {
+    let bubbleView = UIView()
+    let messageLabel = UILabel()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.addSubview(bubbleView)
+        bubbleView.addSubview(messageLabel)
+
+        bubbleView.layer.cornerRadius = 14
+        messageLabel.numberOfLines = 0
+        messageLabel.font = UIFont.systemFont(ofSize: 16)
+        bubbleView.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    func configure(with message: ChatMessage) {
+        messageLabel.text = message.text
+        let isUser = message.isUser
+
+        bubbleView.backgroundColor = isUser ? UIColor.black : UIColor.systemGray6
+        messageLabel.textColor = isUser ? .white : .black
+        messageLabel.textAlignment = isUser ? .right : .left
+
+        // Layout constraints for left/right bubble
+        if isUser {
+            bubbleView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12).isActive = true
+        } else {
+            bubbleView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12).isActive = true
+        }
+
+        NSLayoutConstraint.activate([
+            bubbleView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            bubbleView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+            messageLabel.topAnchor.constraint(equalTo: bubbleView.topAnchor, constant: 8),
+            messageLabel.bottomAnchor.constraint(equalTo: bubbleView.bottomAnchor, constant: -8),
+            messageLabel.leadingAnchor.constraint(equalTo: bubbleView.leadingAnchor, constant: 12),
+            messageLabel.trailingAnchor.constraint(equalTo: bubbleView.trailingAnchor, constant: -12),
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
